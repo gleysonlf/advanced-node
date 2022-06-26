441b57e613e521dee772eb623bc348923467fe1e
Wandgleyson Lira de Figueredo <gleysonlf@gmail.com>
2022-06-25 21:49:14 -0300

    add jest

---
+++ tests/index.spec.ts
@@ -0,0 +1,6 @@
+// confirm that can still process valid tests.
+describe('describe with callback and tests', () => {
+  it('test should pass 1', () => {})
+  it('test should pass 2', () => {})
+  it('test should pass 3', () => {})
+})
